ff6a78e5002621830af1e7e7c7c297c34f5fb542
durgaprasadgajula <107473470+durgaprasadgajula@users.noreply.github.com>
2022-11-10 20:51:36 +0530

    Update index.js
    third

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -3,10 +3,11 @@ import Cookie from 'js-cookie'
 
 const ProtectedRoute = props => {
   const token = Cookie.get('jwt_token')
-  if (token === undefined) {
-    return <Redirect to="/login" />
+  if (token) {
+    return <Route {...props} />
+    
   }
-  return <Route {...props} />
+  return <Redirect to="/login" />
 }
 
 export default ProtectedRoute
